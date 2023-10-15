8844575048d2c1e7d3cdd3e16a061c64113c157b
GTmhyu <zmaula10@gmail.com>
2023-10-15 19:43:18 +0700

    3D Implement

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -11,12 +11,12 @@ const CanvasModel = () => {
           <ambientLight intensity={0.5} />
     <Environment preset="city" />
 
-    {/*<CameraRig>*/}
+    <CameraRig>
       {/*<Background/>*/}
       <Center>
       <Shirt/>
       </Center>
-    {/*</CameraRig>*/}
+    </CameraRig>
     </Canvas>
   )
 }
