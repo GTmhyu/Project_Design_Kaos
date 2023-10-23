d1d5e59e184c4f3239aa1971783cbc595f0297ba
GTmhyu <zmaula10@gmail.com>
2023-10-23 15:08:19 +0700

    3D model Done

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -7,12 +7,18 @@ import Background from "./Background";
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas 
+      shadows
+      // Ubah ukuran Kaos fov
+      camera={{ position: [0, 0, 1.5], fov: 43 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full mas-w-full h-full transition-all ease-in"
+    >
           <ambientLight intensity={0.5} />
     <Environment preset="city" />
 
     <CameraRig>
-      {/*<Background/>*/}
+      <Background/>
       <Center>
       <Shirt/>
       </Center>
--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Tab = () => {
   return (
     <div>Tab</div>
