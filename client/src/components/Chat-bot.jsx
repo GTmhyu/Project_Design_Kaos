4ac614bfc0f30bbb0264cad55230c75f9d253c31
GTmhyu <zmaula10@gmail.com>
2023-12-08 14:19:19 +0700

    s

--- client/src/components/Chat-bot.jsx
+++ client/src/components/Chat-bot.jsx
@@ -6,11 +6,11 @@ class Chat extends Component {
     }
     componentDidMount() {
         (function(d, m){
-<<<<<<< HEAD
+
             var kommunicateSettings = {"appId":"1hbdcatys3hmijklasrioqn5kdj","popupWidget":true,"automaticChatOpenOnNavigation":true};
-=======
+
             var kommunicateSettings = {"appId":"id LU","popupWidget":true,"automaticChatOpenOnNavigation":true};
->>>>>>> d67fa15a6ff0b580635e1995f269c1f565498288
+
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
